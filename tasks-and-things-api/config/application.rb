8121609b0fd855de1f569a23b6736c43e27a083e
Kathleen Kamali <kathleenkamali@gmail.com>
2020-03-11 16:29:44 -0700

    added react

--- tasks-and-things-api/config/application.rb
+++ tasks-and-things-api/config/application.rb
@@ -35,7 +35,7 @@ module TasksAndThingsApi
     config.middleware.insert_before 0, Rack::Cors do
       allow do
           origins '*'
-          resource '*', headers: :any, methods: [:get, :post, :patch]
+          resource '*', headers: :any, methods: [:get, :post, :patch, :delete]
       end
     end
   end
